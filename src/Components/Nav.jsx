b39220cc2e92fed82ae15f62a918ecf01151a28d
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-03-26 21:12:51 -0400

    Updated Nav.jsx

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default Header = () => {
+    return (
+        <nav class="flex place-content-between">
+    
+            <a class="text-lg font-bold" href="/calendar"><h1>Workout Log</h1></a>
+      
+            <ul class="flex gap-6 list-none font-medium w-20">
+              <li><a href="/calendar">Calendar</a></li> 
+              <li><a href="/calendar/createWorkout">Add Workout</a></li>
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+    
+        </nav>
+    )
+}
