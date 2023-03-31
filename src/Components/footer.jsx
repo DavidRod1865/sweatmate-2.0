25138b9b13be382bf29bd17afa0a1c0a99cd435f
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-03-30 20:51:15 -0400

    Updated Footer Component

---
+++ src/Components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p class="text-center mt-20">© 2023 DaveCodes.Tech</p>
+            </footer>
+
+            <script src="/public/js/workout.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        </div>
+    );
+}
